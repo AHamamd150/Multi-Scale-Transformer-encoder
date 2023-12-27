64aeb1279d0d9881d12760ed5b64eca6735aa822
Ahmed Hammad <68282212+AHamamd150@users.noreply.github.com>
2023-12-27 09:14:36 +0900

    Add files via upload

--- input.py
+++ input.py
@@ -1,18 +1,21 @@
-delphes_dir = '/Users/hammad/work/pheno/delphes/'
-sig_dir = '/Users/hammad/work/data/dark_photon/TP1/'
-bkg_dir = '/Users/hammad/work/data/dark_photon/bkg_TP1/bkg1/'
-outdir = '/Users/hammad/work/transformer_package/out/'
-n_cont = 100
-##Hyper parameteres to be tuned
-num_classes=2
-batch_size= 50
-epoch = 5
-num_heads = 5
-hidden_dim= 5
-num_transformers= 3
-n_constit = 50
-n_channels = 7
-input_shape_part = (n_constit,n_channels)
-mlp_units = [128, 64]
-mlp_head_units = [64,n_channels]
-
+sig_file = 'data/sig_J_TP1.npz'
+bkg_file = 'data/bkg1_J_TP1.npz'
+outdir = 'output/'
+##Hyper parameteres to be tuned##
+k_event = 50_000 ### Total number of used events for each signal and background
+test_ratio = 0.2 ## the ratio of the considered test data from the full data set
+attention_mask =True ## Attention mask to be used
+num_classes=2  ## Number of the output classe 
+batch_size= 50  ## Training batch size
+epoch = 3       ## Number of epochs
+num_heads = 5   ## Number of the self attenion heads
+hidden_dim= 5    ## Hidden dimension of the multi-attention heads. Usually is the same as the number of heads. Please look at the tensorflow manual for multi-heads self attention layer
+num_transformers= 3 ## Number of reapted transformer layers
+n_constit = 50     #Number of the particle tokens
+n_channels = 7     # Number of the features of each particle token
+mlp_units = [128, 64]  # number of neurons of the final MLP. if you want to add more FC layers just increase the list, e.g. [128,64,32] accounts for three FC layers
+dropout_rate = 0.2 # Dropout rate to be used
+lr = 0.001 #Learning rate for ADAM otpimizer
+##
+compute_ROC = True #Compute the ROC and the AUC
+save_model = False
--- run.py
+++ run.py
@@ -2,46 +2,68 @@ import os
 #import warnings
 #warnings.filterwarnings("ignore")
 from input import *
-#from prepare_files import *
-from transformer import *
-from train import *
+#from source.prepare_files import *
+from source.transformer import *
+from source.train import *
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import tensorflow as tf
-
+import time
 import keras
 from tensorflow.keras import layers
 import sys
-import sklearn
+import sklearn 
+from sklearn.metrics import auc, roc_curve
 from sklearn.model_selection import train_test_split
 ## check for a GPU
-#if not tf.test.gpu_device_name():
-#   warnings.warn('No GPU found.....')
+if not tf.test.gpu_device_name():
+   warnings.warn('No GPU found.....')
 #  sys.exit()
-#else:
-#   print('Default GPU device :{}'.format(tf.test.gpu_device_name()))
+else:
+   print('Default GPU device :{}'.format(tf.test.gpu_device_name()))
 
 #####
-
-#prepare_inputs(sig_dir,bkg_dir,outdir)
-#print('files stored in: %s' %(outdir))
-signal=np.load(outdir+'signal.npz',allow_pickle=True)['arr_0']
-background =np.load(outdir+'/background.npz',allow_pickle=True)['arr_0']
+signal=np.load(sig_file,allow_pickle=True)['arr_0'][:k_event]
+background =np.load(bkg_file,allow_pickle=True)['arr_0'][:k_event]
 signal =  signal[:,:n_constit,:n_channels ]
 background = background[:,:n_constit,:n_channels ]
 
-print(f'''Shape for the signal= {signal.shape}
-Shape for the background= {background.shape}''')
-
-k = 15_000
-x1_data = np.concatenate((signal[:k], background[:k]))
-y1_data = np.array([1]*len(signal[:k])+[0]*len(background[:k]))
+print(f'''Shape of the signal= {signal.shape}
+Shape of the background= {background.shape}''')
+time.sleep(10)
+x1_data = np.concatenate((signal, background))
+y1_data = np.array([1]*len(signal)+[0]*len(background))
 x_data,y_data= sklearn.utils.shuffle(x1_data, y1_data) # shuffle both 
-X_train, X_test, y_train, y_test = train_test_split(x_data, y_data,shuffle=True, test_size=0.2)
+X_train, X_test, y_train, y_test = train_test_split(x_data, y_data,shuffle=True, test_size=test_ratio)
 x_train= np.array(X_train).astype('float32')
 x_test= np.array(X_test).astype('float32')
 
+print(f'''Shape of the training set= {x_train.shape}
+Shape of the test set= {x_test.shape}''')
+time.sleep(10)
+
 model_part = create_Part_classifier()
 model_part.summary()
+time.sleep(10)
 training_loop(model_part,x_train,y_train,epochs=epoch,batch_size=batch_size)
+test_acc(model_part,X_test,y_test)
+time.sleep(10)
+if save_model:
+  model_part.save(outdir+'/mode.h5')
 
+#############################
+if compute_ROC:
+  import matplotlib.pyplot as plt
+  score=model_part.predict(X_test);
+  fpr, tpr, _ = roc_curve(y_test,score[:,0]);
+  plt.figure(figsize=(6,6))
+  plt.plot([0,1],[0,1],'k--',linewidth=1);
+  plt.plot(tpr,fpr,linewidth=2,label=r'$AUC$= {:.2f}%'.format(float(auc(tpr, fpr))*100));
+  plt.xlabel(r'True positive rate',fontsize=25,c='k');
+  plt.ylabel(r'False positive rate',fontsize=25,c='k');
+  plt.grid(linestyle='--',c='k')
+  plt.legend(loc='best',fontsize=25);
+  plt.tick_params(axis='both',labelsize=25)
+  #plt.yscale('log')
+  plt.tight_layout()
+  plt.show()
 
